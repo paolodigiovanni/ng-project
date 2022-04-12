a987b8bc0041c0cb201e14bff04d4d4ab67f857b
CORP\PADIGIOV <paolo.digiovanni@capgemini.com>
2022-04-12 22:58:59 +0200

    layout module + split header/user-card

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SharedModule } from '../shared/shared.module';
+import { UserCardComponent } from './user-card/user-card.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    UserCardComponent
   ],
   imports: [
     SharedModule
